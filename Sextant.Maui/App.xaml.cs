6f08f1925968639d0823fa6f1a640671a01ee72c
Vivek Saurabh <saurabhvivek30@gmail.com>
2024-09-22 22:24:28 +0530

    added namespaces data

--- Sextant.Maui/App.xaml.cs
+++ Sextant.Maui/App.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace Sextant.Maui
+﻿using k8s;
+
+namespace Sextant.Maui
 {
     public partial class App : Application
     {
+        public static Kubernetes? Kubernetes { get; set; }
         public App()
         {
             InitializeComponent();
+            var config = KubernetesClientConfiguration.BuildDefaultConfig();
+            Kubernetes = new Kubernetes(config);
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- Sextant.Maui/Views/Workloads/Namespaces.xaml.cs
+++ Sextant.Maui/Views/Workloads/Namespaces.xaml.cs
@@ -1,3 +1,5 @@
+using k8s;
+
 namespace Sextant.Maui.Views.Workloads;
 
 public partial class Namespaces : ContentPage
@@ -5,5 +7,19 @@ public partial class Namespaces : ContentPage
 	public Namespaces()
 	{
 		InitializeComponent();
-	}
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if(App.Kubernetes is null) return;
+        var namespaceList = await App.Kubernetes.CoreV1.ListNamespaceAsync();
+        namespacesTbl.Clear();
+        foreach (var item in namespaceList.Items)
+        {
+            namespacesTbl.Add(new TextCell() { Text = item.Metadata.Name, Detail = item.Status.Phase });
+        }
+
+    }
+
 }
